4d78d6977ed36c85b922a187a4b138d17db43678
Yura <yurapostevka@gmail.com>
2017-03-21 18:02:26 +0200

    unnecessary

---
+++ dbsApiService.ts
@@ -0,0 +1,33 @@
+﻿import { Http, Response, Headers } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+import { IField } from './IField';
+
+@Injectable()
+export class DbsApiService {
+
+    constructor(private _http: Http) {
+    }
+
+    fields: IField[] = [];
+
+    public getFields = (actionUrl): IField[] => {
+        this._http.get(actionUrl).map(data => data.json()).subscribe(data => this.fields = data);
+
+        return this.fields;
+    }
+
+    public saveMappings = (fields: IField[]) => {
+        let url = 'http://localhost:2606/home/SaveMappings';
+
+        console.log(JSON.stringify(fields));
+
+        let headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+        return this._http.post(url, fields, { headers: headers }).subscribe();
+    }
+        
+}
